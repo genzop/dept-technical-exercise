eb7fdf03564caafcc812b20d90ac6ab90ed74bba
Enzo Panettieri <genzop@gmail.com>
2022-12-01 21:49:16 -0300

    Added router and basic routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import logo from "./logo.svg";
-
-import "./App.css";
+import "./styles/globals.scss";
+import HomePage from "./pages/home";
+import FavouritesPage from "./pages/favourites";
+import LaunchPage from "./pages/launch";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/favourites",
+      element: <FavouritesPage />,
+    },
+    {
+      path: "/launch/:launchId",
+      element: <LaunchPage />,
+      loader: async ({ request, params }) => {
+        return params;
+      },
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 };
---
+++ src/pages/launch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const LaunchPage = () => {
+  const params = useLoaderData();
+  const launchId = params.launchId;
+
+  return <div>Launch Page</div>;
+};
+
+export default LaunchPage;
